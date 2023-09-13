567019f3f4a8888742c7a92491c2ff72f59722c0
bulby97 <s.montlouis.calixte@gmail.com>
2023-09-13 13:01:24 +0200

    add normal app.js

--- App.js
+++ App.js
@@ -1,35 +1,21 @@
-import { StatusBar } from 'expo-status-bar'
-import { Provider } from 'jotai'
-import { StyleSheet, Text, View } from 'react-native'
-import { Component } from './src/Component'
-import { Suspense } from 'react'
-import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { SafeAreaView } from 'react-native-safe-area-context'
 import { PortalProvider } from '@gorhom/portal'
+import { Provider } from 'jotai'
+import { Suspense } from 'react'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import Page from './src/app'
 
 export default function App() {
   return (
     <SafeAreaProvider>
       <Provider>
         <Suspense fallback={null}>
-          <SafeAreaView style={{ flex: 1 }}>
-            <View style={styles.container}>
-              <Text>Open up App.js to start working on your app!</Text>
-              <Component />
-              <StatusBar style="auto" />
-            </View>
-          </SafeAreaView>
+          <PortalProvider>
+            <SafeAreaView style={{ flex: 1 }}>
+              <Page />
+            </SafeAreaView>
+          </PortalProvider>
         </Suspense>
       </Provider>
     </SafeAreaProvider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-})
